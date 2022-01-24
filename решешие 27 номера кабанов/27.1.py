6448cf5d105e028efdfc08d598390f90b2ef70c8
Vadim Steshkin <71772016+vadimsteshkin@users.noreply.github.com>
2022-01-24 18:23:15 +0300

    None

---
+++ решешие 27 номера кабанов/27.1.py
@@ -0,0 +1,13 @@
+k=open('27-B_1.txt')
+m=0
+b=[]
+for _ in range(int(k.readline())):
+    m1=list(map(int,k.readline().split()))
+    m+=max(m1)
+    b.append(max(m1) - min(m1))
+k.close()
+b=sorted(b)
+for i in b:
+    if (m-i)%3!=0:
+        print(m-i)
+        break
